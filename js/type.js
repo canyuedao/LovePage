eb9613aa89e7e6066ea2169d39be1125eb1d48b3
ChenXin <xxwd001@gmail.com>
2012-06-04 20:24:44 +0800

    Update master

---
+++ js/type.js
@@ -0,0 +1,41 @@
+var getID=document.getElementById("love"); 
+var text=getID.innerHTML;
+var speed=80;
+var jj=-1;var kk=0; var wds=0;
+function typeOut(){
+  ++jj;
+	if (kk == 0 && text.charAt(jj) == "<") {
+	    kk = 1;
+	    wds = 0;
+	}
+	else if (kk == 1 && text.charAt(jj) == ">") {
+	    kk = 0;
+	    wds = 0;
+	}
+	else if (kk == 1) {
+	    wds = 0;
+	}
+	if (kk == 0 && text.charAt(jj) != ">" && jj <= text.length) {
+	    wds = 1;
+        getID.innerHTML = text.substr(0, jj);
+    }
+    if (wds >0 && jj <= text.length) {
+  
+        setTimeout("typeOut()", speed);
+    }
+    else if (jj <= text.length) {
+        typeOut();
+    }
+}
+
+
+
+    if (document.getElementById) {
+
+        getID.style.visibility="visible";   
+        typeOut();
+        var removeBox=document.getElementById("box");
+        speedTime=(text.length)*80;
+        setTimeout("removeBox.parentNode.removeChild(removeBox)",(speedTime-4000));
+       
+    }          
